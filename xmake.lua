0fba1ff3883c317ad4e5be0cc5aa481048ecc534
shad0wshayd3 <2724172+shad0wshayd3@users.noreply.github.com>
2026-08-30 04:41:29 -0600

    revert: release impl

--- xmake.lua
+++ xmake.lua
@@ -30,7 +30,3 @@ target("BakaPowerArmorHUD")
 
     -- add extra files
     add_extrafiles(".clang-format")
-
-    -- add install files
-    add_installfiles("res/Interface/*.swf", { prefixdir = "Interface" })
-    add_installfiles("res/MCM/**.*",        { prefixdir = "MCM/config/BakaPowerArmorHUD" })
